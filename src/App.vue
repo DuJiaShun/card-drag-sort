9cd27439e59fc9ee7f550882a22c58dd5375030e
dujiashun <15961756284@163.com>
2024-06-17 17:57:57 +0800

    build:优化项目依赖

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
     <card-drag-sort :data="data" @drag-stop="dragStop" :duration="500">
-      <template #content="{ card }">
+      <!-- <template #content="{ card }">
         <div v-if="card.id === 'one'"></div>
-      </template>
+      </template> -->
     </card-drag-sort>
   </div>
 </template>
 <script setup lang="ts">
 import { reactive } from 'vue'
-// import { CardDragSort } from '../dist/card-drag-sort.js'
-import { CardDragSort } from '../package/index'
+// 打包后测试
+import { CardDragSort } from '../dist/card-drag-sort.js'
+// 开发
+// import { CardDragSort } from '../package/index'
 const data = reactive([
   { title: '卡片标题1', id: 'one' },
   { title: '卡片标题2', id: 'two' },
